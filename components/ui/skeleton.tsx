8ce054a50ad2da380ccdd5a8da91b3870ba910a8
tahavirdiwala <taha@redefinesolutions.com>
2025-05-29 15:23:57 +0530

    adding types for components

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,15 +1,14 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+interface ISkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+function Skeleton({ className, ...props }: ISkeletonProps) {
   return (
     <div
       className={cn("animate-pulse rounded-md bg-muted", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
