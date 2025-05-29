fd74e68eee3a28be6f29338f6edf6ada4bbb9f81
tahavirdiwala <taha@redefinesolutions.com>
2025-05-29 14:55:24 +0530

    fix issues

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,5 +1,4 @@
 import useCountdown from "@/hooks/useCountDown";
 import { useIsMobile } from "@/hooks/use-mobile";
-import { useToast } from "@/hooks/use-toast";
 
-export { useCountdown, useIsMobile, useToast };
+export { useCountdown, useIsMobile };
