95b5fe041b234c356cffaa4801da7b1964863471
tahavirdiwala <taha@redefinesolutions.com>
2025-05-30 11:54:20 +0530

    chore/remove-usemobile-hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,5 +1,4 @@
 import { useCountdown } from "@/hooks/useCountDown";
-import { useIsMobile } from "@/hooks/useMobile";
 import { useMaintenance } from "@/hooks/useMaintenance";
 
-export { useCountdown, useIsMobile, useMaintenance };
+export { useCountdown, useMaintenance };
