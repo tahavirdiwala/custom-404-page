e010c576d4e6e46cd60df0f187015a30c54d636d
tahavirdiwala <taha@redefinesolutions.com>
2025-05-30 10:16:19 +0530

    remove redundant imports

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,5 +1,5 @@
 "use client";
-import * as React from "react";
+import React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
