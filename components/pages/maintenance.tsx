93d0ef00a08ae6306ae365101d66971da80cc860
tahavirdiwala <taha@redefinesolutions.com>
2025-05-30 11:50:46 +0530

    remove unnecessary imports

--- components/pages/maintenance.tsx
+++ components/pages/maintenance.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
